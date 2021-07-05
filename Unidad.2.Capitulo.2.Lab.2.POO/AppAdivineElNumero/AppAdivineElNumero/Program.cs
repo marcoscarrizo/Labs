fed2de005909253cfdd6776a0a3d649d3f5277cf
Marcos <marcoscarrizo1410@gmail.com>
2021-07-05 20:22:00 -0300

    Agregar Juego y JuegoConAyuda

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/AppAdivineElNumero/AppAdivineElNumero/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace AppAdivineElNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Juego j = new Juego();
+            j.ComenzarJuego();
+        }
+    }
+}
