41853eb746fe98d6a1614f3f9786307d2d49c4a4
Marcos <marcoscarrizo1410@gmail.com>
2021-07-05 21:46:55 -0300

    Agregar consulta Provincias

---
+++ Unidad.2.Extra.Lab.3.LINQ/AppProvincias/AppProvincias/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppProvincias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] provincias = {"Santa Fe", "Buenos Aires", "Córdoba", "Corrientes", "Entre Rios", "Tucumán", "Santiago del Estero", "Jujuy", "Salta", "Misiones", "Chaco", "Mendoza", "La Rioja", "La Pampa", "Chubut", "Rio Negro", "Formosa", "Tierra del Fuego" };
+
+            Console.WriteLine("Seleccione:");
+            Console.WriteLine("1) Provincias que empiezan con S");
+            Console.WriteLine("2) Provincias que empiezan con T");
+            
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    var ProvinciasConS = from provincia in provincias
+                                         where provincia.StartsWith("S")
+                                         select provincia;
+
+                    foreach (var p in ProvinciasConS)
+                    {
+                        Console.WriteLine(p);
+                    }
+                    break;
+                case 2:
+                    var ProvinciasConT = from provincia in provincias
+                                         where provincia.StartsWith("T")
+                                         select provincia;
+
+                    foreach (var p in ProvinciasConT)
+                    {
+                        Console.WriteLine(p);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+    }
+}
